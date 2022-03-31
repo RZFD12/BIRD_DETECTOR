e4674782f333ce809268a0c133f843abfc57122c
x0nEAxe <andrey-bagurin@yandex.ru>
2022-03-31 08:26:14 +0300

    ui upd

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created by: Qt User Interface Compiler version 5.15.0
+** Created by: Qt User Interface Compiler version 5.15.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -63,20 +63,20 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(1900, 1080);
+        MainWindow->resize(1920, 1080);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         graphicsView = new QGraphicsView(centralwidget);
         graphicsView->setObjectName(QString::fromUtf8("graphicsView"));
-        graphicsView->setGeometry(QRect(10, 9, 931, 791));
+        graphicsView->setGeometry(QRect(10, 9, 941, 791));
         graphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
         graphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
         graphicsView_2 = new QGraphicsView(centralwidget);
         graphicsView_2->setObjectName(QString::fromUtf8("graphicsView_2"));
-        graphicsView_2->setGeometry(QRect(959, 9, 931, 791));
+        graphicsView_2->setGeometry(QRect(970, 10, 941, 791));
         horizontalLayoutWidget = new QWidget(centralwidget);
         horizontalLayoutWidget->setObjectName(QString::fromUtf8("horizontalLayoutWidget"));
-        horizontalLayoutWidget->setGeometry(QRect(310, 850, 191, 71));
+        horizontalLayoutWidget->setGeometry(QRect(860, 840, 201, 41));
         horizontalLayout = new QHBoxLayout(horizontalLayoutWidget);
         horizontalLayout->setSpacing(0);
         horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
@@ -98,23 +98,24 @@ public:
 
         lineEdit = new QLineEdit(centralwidget);
         lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
-        lineEdit->setGeometry(QRect(70, 800, 871, 21));
+        lineEdit->setGeometry(QRect(70, 800, 881, 21));
         lineEdit_2 = new QLineEdit(centralwidget);
         lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
-        lineEdit_2->setGeometry(QRect(1020, 800, 871, 21));
+        lineEdit_2->setGeometry(QRect(1030, 800, 881, 21));
         label = new QLabel(centralwidget);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(970, 800, 41, 21));
+        label->setGeometry(QRect(980, 800, 41, 21));
         label_2 = new QLabel(centralwidget);
         label_2->setObjectName(QString::fromUtf8("label_2"));
         label_2->setGeometry(QRect(20, 800, 47, 21));
         groupBox = new QGroupBox(centralwidget);
         groupBox->setObjectName(QString::fromUtf8("groupBox"));
-        groupBox->setGeometry(QRect(10, 830, 291, 91));
+        groupBox->setGeometry(QRect(20, 830, 291, 101));
+        groupBox->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter);
         groupBox->setFlat(true);
         formLayoutWidget = new QWidget(groupBox);
         formLayoutWidget->setObjectName(QString::fromUtf8("formLayoutWidget"));
-        formLayoutWidget->setGeometry(QRect(9, 20, 271, 42));
+        formLayoutWidget->setGeometry(QRect(10, 30, 271, 42));
         formLayout = new QFormLayout(formLayoutWidget);
         formLayout->setObjectName(QString::fromUtf8("formLayout"));
         formLayout->setContentsMargins(0, 0, 0, 0);
@@ -146,7 +147,7 @@ public:
 
         horizontalLayoutWidget_2 = new QWidget(groupBox);
         horizontalLayoutWidget_2->setObjectName(QString::fromUtf8("horizontalLayoutWidget_2"));
-        horizontalLayoutWidget_2->setGeometry(QRect(9, 60, 271, 31));
+        horizontalLayoutWidget_2->setGeometry(QRect(10, 70, 271, 31));
         horizontalLayout_2 = new QHBoxLayout(horizontalLayoutWidget_2);
         horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
         horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
@@ -167,19 +168,19 @@ public:
 
         lineEdit_3 = new QLineEdit(centralwidget);
         lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
-        lineEdit_3->setGeometry(QRect(510, 850, 211, 21));
+        lineEdit_3->setGeometry(QRect(20, 960, 211, 21));
         lineEdit_4 = new QLineEdit(centralwidget);
         lineEdit_4->setObjectName(QString::fromUtf8("lineEdit_4"));
-        lineEdit_4->setGeometry(QRect(510, 880, 211, 21));
+        lineEdit_4->setGeometry(QRect(20, 990, 211, 21));
         toolButton = new QToolButton(centralwidget);
         toolButton->setObjectName(QString::fromUtf8("toolButton"));
-        toolButton->setGeometry(QRect(730, 850, 41, 20));
+        toolButton->setGeometry(QRect(240, 960, 41, 20));
         toolButton_2 = new QToolButton(centralwidget);
         toolButton_2->setObjectName(QString::fromUtf8("toolButton_2"));
-        toolButton_2->setGeometry(QRect(730, 880, 41, 20));
+        toolButton_2->setGeometry(QRect(240, 990, 41, 20));
         progressBar = new QProgressBar(centralwidget);
         progressBar->setObjectName(QString::fromUtf8("progressBar"));
-        progressBar->setGeometry(QRect(960, 870, 921, 23));
+        progressBar->setGeometry(QRect(1300, 980, 611, 23));
         progressBar->setValue(0);
         MainWindow->setCentralWidget(centralwidget);
 
